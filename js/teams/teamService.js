306066499ce086110f7c72b7a6c75fbf307c8f3b
Blake Hagen <blakehagen@gmail.com>
2015-11-02 08:39:56 -0700

    teamService addNewGame

--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -3,5 +3,31 @@ var app = angular.module('nbaRoutes');
 app.service('teamService', function ($http, $q) {
 
     // service code
+    
+    this.addNewGame = function(gameObj){
+        var url = 'https://api.parse.com/1/classes/' + gameObj.homeTeam;
+        if(parseInt(gameObj.homeTeamScore) > parseInt(gameObj.opponentScore)){
+            gameObj.won = true;
+        } else {
+            gameObj.won = false;
+        }
+        return $http({
+            method: 'POST',
+            url: url,
+            data: gameObj
+        });
+    };
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 });
