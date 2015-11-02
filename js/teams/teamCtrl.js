420c3b02e500f17e080800d747b095bc9035d079
Blake Hagen <blakehagen@gmail.com>
2015-11-02 12:38:39 -0700

    adding data to team tmpl

--- js/teams/teamCtrl.js
+++ js/teams/teamCtrl.js
@@ -1,8 +1,50 @@
 var app = angular.module('nbaRoutes');
 
 // this controller needs to be injected the resolved data from the router
-app.controller('teamCtrl', function ($scope, $stateParams, teamService) {
+app.controller('teamCtrl', function ($scope, $stateParams, teamService, teamData) {
 
     // controller code
+    
+    $scope.teamData = teamData;
+    
+    $scope.newGame = {
+        homeTeam: $scope.homeTeam.split(' ').join('').toLowerCase()
+    };
+
+    $scope.showNewGameForm = false;
+
+    $scope.toggleNewGameForm = function () {
+        $scope.showNewGameForm = !$scope.showNewGameForm;
+    }
+
+    if ($stateParams.team === 'utahjazz') {
+        $scope.homeTeam = 'Utah Jazz';
+        $scope.logoPath = 'images/jazz-logo.png';
+    }
+
+    if ($stateParams.team === 'losangeleslakers') {
+        $scope.homeTeam = 'Los Angeles Lakers';
+        $scope.logoPath = 'images/lakers-logo.png';
+    }
+
+    if ($stateParams.team === 'miamiheat') {
+        $scope.homeTeam = 'Miami Heat';
+        $scope.logoPath = 'images/heat-logo.png';
+    }
+
+    $scope.submitGame = function () {
+        teamService.addNewGame($scope.newGame).then(function(){
+            teamService.getTeamData($scope.newGame.homeTeam).then(function(data){
+                $scope.teamData = data;
+                $scope.newGame = {};
+                $scope.showNewGameForm = false;
+            })
+        })
+
+    };
+    
+
+
+
 
 });
--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -4,9 +4,9 @@ app.service('teamService', function ($http, $q) {
 
     // service code
     
-    this.addNewGame = function(gameObj){
+    this.addNewGame = function (gameObj) {
         var url = 'https://api.parse.com/1/classes/' + gameObj.homeTeam;
-        if(parseInt(gameObj.homeTeamScore) > parseInt(gameObj.opponentScore)){
+        if (parseInt(gameObj.homeTeamScore) > parseInt(gameObj.opponentScore)) {
             gameObj.won = true;
         } else {
             gameObj.won = false;
@@ -17,17 +17,39 @@ app.service('teamService', function ($http, $q) {
             data: gameObj
         });
     };
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+    this.getTeamData = function (team) {
+        var deferred = $q.defer();
+        $http({
+            method: 'GET',
+            url: 'https://api.parse.com/1/classes/' + team
+        }).then(function (data) {
+            var results = data.data.results;
+            var wins = 0;
+            var losses = 0;
+            for (var i = 0; i < results.length; i++) {
+                if (results[i].won === true) {
+                    wins++;
+                } else {
+                    losses++;
+                }
+            }
+            results.wins = wins;
+            results.losses = losses;
+            deferred.resolve(results);
+            console.log(results);
+        })
+        return deferred.promise;
+    };
+
+
+
+
+
+
+
+
+
+
 
 });
